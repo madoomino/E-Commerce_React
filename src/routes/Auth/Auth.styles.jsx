c15187d939261855ff2b6cc2e613bad703d2f600
Madoo Mino <sasa6521d@gmail.com>
2023-01-04 20:54:57 +0200

    fixing forms styling

--- src/routes/Auth/Auth.styles.jsx
+++ src/routes/Auth/Auth.styles.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AuthenticationContainer = styled.div`
   display: flex;
-  width: 60%;
+  width: 65%;
   justify-content: space-between;
   margin: 30px auto;
 `;
